a9df2595ebed2d34a08bf4608ccb68f0f3efc972
Timur <timengalytchev@mail.ru>
2024-11-29 16:40:46 +0300

    feat(widget): made header

--- src/app/providers/router/routeConfig/routeConfig.tsx
+++ src/app/providers/router/routeConfig/routeConfig.tsx
@@ -1,9 +1,8 @@
 import { NotFoundPage } from "@/pages/NotFoundPage";
 import { LazyMainPage } from "@/pages/MainPage";
-import { AppRoutes, getRouteLogin, getRouteMain, getRouteNotFound, getRouteRegister } from "@/shared/consts/router";
+import { AppRoutes, getRouteLogin, getRouteMain, getRouteNotFound } from "@/shared/consts/router";
 import { AppRouteProps } from "@/shared/types/router";
 import { LazyLoginPage } from "@/pages/Login";
-import { LazyRegisterPage } from "@/pages/Register";
 
 export const routeConfig: Record<AppRoutes, AppRouteProps> = {
 	[AppRoutes.MAIN]: {
@@ -17,9 +16,5 @@ export const routeConfig: Record<AppRoutes, AppRouteProps> = {
 	[AppRoutes.LOGIN]: {
 		path: getRouteLogin(),
 		element: <LazyLoginPage />,
-	},
-	[AppRoutes.REGISTER]: {
-		path: getRouteRegister(),
-		element: <LazyRegisterPage />,
 	},
 };

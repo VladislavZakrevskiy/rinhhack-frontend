539ad9d132487de7098dca425985a37aaf9937bf
Timur <timengalytchev@mail.ru>
2024-12-01 11:05:44 +0300

    fix(page): excel page with sockets

--- src/pages/ExcelPage/ui/ExcelPage.tsx
+++ src/pages/ExcelPage/ui/ExcelPage.tsx
@@ -24,6 +24,9 @@ const ExcelPage: React.FC = () => {
 	const [isFileLoaded, setIsFileLoaded] = useState<boolean>(false);
 	const [fileUrl, setFileUrl] = useState<string | null>(null);
 
+  const pathname = window.location.pathname;
+  const filename = pathname.split('/').pop();
+
 	const sendToSocket = (newData: SpreadsheetData) => {
 		try {
 			const excelBuffer = generateExcelBufferFromData(newData);
@@ -37,10 +40,9 @@ const ExcelPage: React.FC = () => {
 
 			const base64File = arrayBufferToBase64(excelBuffer);
 			// console.log("Sending modified file in Base64 format:", base64File);
-
 			// console.log("emit");
 			socket?.emit("upload_file", {
-				filename: "test.xlsx",
+				filename: filename,
 				data: base64File,
 			});
 		} catch (error) {
@@ -50,42 +52,44 @@ const ExcelPage: React.FC = () => {
 
 	const debouncedSendSocket = useDebounce(sendToSocket, 500);
 
+
+
 	useEffect(() => {
+    const token = localStorage.getItem("access_token");
 		const socketConnection = io("http://pepper-coding.online", {
 			transports: ["websocket"],
-		});
+      auth: {
+        token: `Bearer ${token}`,
+      },
+      });
+      console.log(token)
 		setSocket(socketConnection);
 
 		socketConnection.on("connect", () => {
 			// console.log("Socket connected: ", socketConnection.id);
-			socketConnection.emit("get_file");
+      console.log(filename)
+			socketConnection.emit("get_file", { filename });
 		});
 
-		socketConnection.on("file_update", async (message: WebSocketMessage) => {
-			// console.log(message.data);
-			if (message.data) {
-				try {
-					// console.log("Received data:", message.data);
-
-					const decodedFile = decodeBase64ToArrayBuffer(message.data);
-					if (decodedFile) {
-						// console.log("File successfully decoded into ArrayBuffer:", decodedFile);
-
-						const fileDownloadUrl = URL.createObjectURL(
-							new Blob([decodedFile], {
-								type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-							}),
-						);
-						setFileUrl(fileDownloadUrl);
-						await readExcelFile(decodedFile);
-					} else {
-						console.error("Error: file could not be decoded.");
-					}
-				} catch (error) {
-					console.error("Error decoding file:", error);
-				}
-			}
-		});
+    socketConnection.on("file_update", async (message: WebSocketMessage) => {
+      if (message.data) {
+          try {
+              const decodedFile = decodeBase64ToArrayBuffer(message.data);
+              if (decodedFile) {
+                  await readExcelFile(decodedFile);
+                  
+                  const fileDownloadUrl = URL.createObjectURL(
+                      new Blob([decodedFile], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
+                  );
+                  setFileUrl(fileDownloadUrl);
+              } else {
+                  console.error("Error: file could not be decoded.");
+              }
+          } catch (error) {
+              console.error("Error decoding file:", error);
+          }
+      }
+  });
 
 		socketConnection.on("error", (message: WebSocketMessage) => {
 			if (message.message) {
@@ -98,76 +102,86 @@ const ExcelPage: React.FC = () => {
 		};
 	}, []);
 
-	const handleCellChange = (newData: SpreadsheetData) => {
-		// console.log("handleCellChange");
-		if (JSON.stringify(newData) === JSON.stringify(data)) {
-			return;
-		}
-		if (!isFileLoaded) {
-			console.error("File is not loaded yet. Changes cannot be sent.");
-			return;
-		}
+  const handleCellChange = (newData: SpreadsheetData) => {
+    if (JSON.stringify(newData) === JSON.stringify(data)) {
+      return;
+    }
+    if (!isFileLoaded) {
+      console.error("File is not loaded yet. Changes cannot be sent.");
+      return;
+    }
 
-		setData(newData);
-		debouncedSendSocket(newData);
-	};
+    setData(newData);
+
+    debouncedSendSocket(newData);
+  };
+  
 
 	const readExcelFile = async (arrayBuffer: ArrayBuffer) => {
-		try {
-			const workbook = XLSX.read(arrayBuffer, { type: "array", cellDates: true });
-			const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-			if (!worksheet) {
-				console.error("No sheet found in workbook.");
-				return;
-			}
+    try {
+      const workbook = XLSX.read(arrayBuffer, { type: "array", cellDates: true });
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      if (!worksheet) {
+        console.error("No sheet found in workbook.");
+        return;
+      }
+  
+      const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      const updatedData = addEmptyRowAndColumn(parsedData);
+  
+      // @ts-ignore
+      const matrix = updatedData.map((row) => row.map((cell) => ({ value: cell }) as CellBase));
+  
+      setData(matrix);
+      setIsFileLoaded(true);
+    } catch (error) {
+      console.error("Error reading or parsing Excel file:", error);
+    }
+  };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const addEmptyRowAndColumn = (data: any[]) => {
+    data.push(new Array(data[0].length).fill(""));
+    data.forEach(row => row.push(""));
+    return data;
+  };
 
-			const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-			// console.log("Parsed Excel data:", parsedData);
+  return (
+    <>
+        <Header />
+        <Stack
+            horizontalAlign="center"
+            style={{
+                height: "100vh",
+                padding: "20px",
+            }}
+        >
+            <Card className="w-full min-h-[50vh]">
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                    <Title3>Редактирование файла</Title3>
+                    <ExcelModal />
+                </div>
 
-			// @ts-ignore
-			const matrix = parsedData.map((row) => row.map((cell) => ({ value: cell }) as CellBase));
+                <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                    {isFileLoaded ? (
+                        <ExcelTable data={data} setData={setData} handleCellChange={handleCellChange} />
+                    ) : (
+                        <Spinner size="large" />
+                    )}
+                </div>
+                {fileUrl && (
+                    <div>
+                        <a href={fileUrl} download={filename} style={{ textDecoration: 'none' }}>
+                            <Button>
+                                {t("download new")}
+                            </Button>
+                        </a>
+                    </div>
+                )}
+            </Card>
+        </Stack>
+    </>
+);
 
-			setData(matrix);
-			setIsFileLoaded(true);
-		} catch (error) {
-			console.error("Error reading or parsing Excel file:", error);
-		}
-	};
-
-	return (
-		<>
-			<Header />
-			<Stack
-				horizontalAlign="center"
-				style={{
-					height: "100vh",
-					padding: "20px",
-				}}
-			>
-				<Card className="w-full min-h-[50vh]">
-					<div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-						<Title3>Редактирование файла</Title3>
-						<ExcelModal />
-					</div>
-
-					<div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-						{isFileLoaded ? (
-							<ExcelTable data={data} setData={setData} handleCellChange={handleCellChange} />
-						) : (
-							<Spinner size="large" />
-						)}
-					</div>
-					{fileUrl && (
-						<div>
-							<Button href={fileUrl} download="modified_file.xlsx">
-								{t("download new")}
-							</Button>
-						</div>
-					)}
-				</Card>
-			</Stack>
-		</>
-	);
 };
 
 export default ExcelPage;
